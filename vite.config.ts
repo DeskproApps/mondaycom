00da429113555e92b9c6c6b0dce173adf9d7024c
OJ Abba <phavorabba@gmail.com>
2025-02-25 12:46:48 +0000

    [Chore] Add absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import copy from "rollup-plugin-copy";
+import path from "path";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +10,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: true,
+  },
+  resolve:{
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
   build: {
     rollupOptions: {
