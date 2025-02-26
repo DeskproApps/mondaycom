ebb0559666ca4d1016b04a62c758e05b42f57227
OJ Abba <phavorabba@gmail.com>
2025-02-26 13:33:07 +0000

    [Fix] Update imports in tests after moving API functions

--- tests/pages/Create/CreateNote.test.tsx
+++ tests/pages/Create/CreateNote.test.tsx
@@ -1,6 +1,6 @@
 import { lightTheme, ThemeProvider } from "@deskpro/deskpro-ui";
 import { cleanup, fireEvent, render, waitFor } from "@testing-library/react/";
-import * as Api from "../../../src/api/api";
+import createNote from "../../../src/api/monday/createNote";
 import React from "react";
 import { CreateNote } from "../../../src/pages/Create/Note";
 
@@ -12,11 +12,7 @@ const renderPage = () => {
   );
 };
 
-jest.mock("../../../src/api/api", () => {
-  return {
-    createNote: jest.fn(),
-  };
-});
+jest.mock("../../../src/api/monday/createNote", () => jest.fn());
 
 describe("Create Note", () => {
   test("Creating a note should work correctly", async () => {
@@ -29,7 +25,7 @@ describe("Create Note", () => {
     fireEvent.click(getByTestId("button-submit"));
 
     await waitFor(() => {
-      expect(Api.createNote).toHaveBeenCalledTimes(1);
+      expect(createNote).toHaveBeenCalledTimes(1);
     });
   });
 
@@ -39,7 +35,7 @@ describe("Create Note", () => {
     fireEvent.click(getByTestId("button-submit"));
 
     await waitFor(() => {
-      expect(Api.createNote).toHaveBeenCalledTimes(0);
+      expect(createNote).toHaveBeenCalledTimes(0);
     });
   });
 
--- tests/pages/Main.test.tsx
+++ tests/pages/Main.test.tsx
@@ -1,7 +1,7 @@
-import { lightTheme, ThemeProvider } from "@deskpro/deskpro-ui";
 import { cleanup, render, waitFor } from "@testing-library/react/";
-import React from "react";
+import { lightTheme, ThemeProvider } from "@deskpro/deskpro-ui";
 import { Main } from "../../src/pages/Main";
+import React from "react";
 
 const renderPage = () => {
   return render(
@@ -11,35 +11,29 @@ const renderPage = () => {
   );
 };
 
-jest.mock("../../src/api/api", () => {
-
-  return {
-    getItemsById: () => [
-      {
-        id: "123456",
-        name: "Fake Item",
-        state: "active",
-        created_at: "2025-01-16T17:10:26Z",
-        group: {
-          id: "topics",
-          name: "Epic Group"
-        },
-        board: {
-          id: "1234565789",
-          name: "DeskproBoard",
-        },
-        workspace: "Epic Workspace",
-        column_values: [],
-        creator: {
-          id: "789",
-          name: "John Doe"
-        },
-        updates: [],
-      },
-    ],
-  };
-
-});
+jest.mock("../../src/api/monday/getItemsById", () => () => [
+  {
+    id: "123456",
+    name: "Fake Item",
+    state: "active",
+    created_at: "2025-01-16T17:10:26Z",
+    group: {
+      id: "topics",
+      name: "Epic Group"
+    },
+    board: {
+      id: "1234565789",
+      name: "DeskproBoard",
+    },
+    workspace: "Epic Workspace",
+    column_values: [],
+    creator: {
+      id: "789",
+      name: "John Doe"
+    },
+    updates: [],
+  },
+]);
 
 describe("Main", () => {
   test("Main page should show all data correctly", async () => {
