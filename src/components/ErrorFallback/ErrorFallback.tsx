9a8d50db0d548ce3072e5c79b72640d528a57850
Paul <1740195+HappyPaul55@users.noreply.github.com>
2025-06-16 17:49:31 +0100

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry (#41)
    * Integrated Sentry for better error tracking in apps.
    
    * Added necessary Sentry configuration settings.

--- src/components/ErrorFallback/ErrorFallback.tsx
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -2,21 +2,19 @@ import { faRefresh } from "@fortawesome/free-solid-svg-icons";
 
 import { AnyIcon, Button, H1, H2, Stack } from "@deskpro/deskpro-ui";
 import { parseJsonErrorMessage } from "../../utils/utils";
+import { FallbackRender } from "@sentry/react";
 
-export const ErrorFallback = ({
+export const ErrorFallback: FallbackRender = ({
   error,
-  resetErrorBoundary,
-}: {
-  error: Error;
-  resetErrorBoundary: () => void;
+  resetError,
 }) => {
   return (
     <Stack vertical gap={10} role="alert">
       <H1>Something went wrong:</H1>
-      <H2>{parseJsonErrorMessage(error.message)}</H2>
+      <H2>{parseJsonErrorMessage((error as Error).message)}</H2>
       <Button
         text="Reload"
-        onClick={resetErrorBoundary}
+        onClick={resetError}
         icon={faRefresh as AnyIcon}
         intent="secondary"
       />
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import './instrument';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -5,10 +6,11 @@ import { DeskproAppProvider } from "@deskpro/app-sdk";
 import "./main.css";
 import "simplebar/dist/simplebar.min.css";
 import { Scrollbar } from "@deskpro/deskpro-ui";
+import { reactErrorHandler } from '@sentry/react';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
+  onRecoverableError: reactErrorHandler(),
+});
 root.render(
   <React.StrictMode>
     <Scrollbar style={{ height: "100%", width: "100%" }}>
